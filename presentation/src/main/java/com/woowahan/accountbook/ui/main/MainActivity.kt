36d13b1a609db3c49a614bc68001bd54258360e1
youngmu <tyg04308@naver.com>
2022-07-27 21:08:09 +0900

    [FEAT] #1 - TopAppBar

---
+++ presentation/src/main/java/com/woowahan/accountbook/ui/main/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.woowahan.accountbook.ui.main
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.woowahan.accountbook.R
+import com.woowahan.accountbook.ui.component.AccountBookTopAppBar
+import com.woowahan.accountbook.ui.theme.AccountBookTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AccountBookTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Scaffold(
+                        topBar = {
+                            AccountBookTopAppBar(
+                                title = "Good",
+                                btn1Image = R.drawable.ic_left,
+                                btn1OnClick = {
+                                    Toast.makeText(this, "btn1", Toast.LENGTH_SHORT).show()
+                                },
+                                btn2Image = R.drawable.ic_right,
+                                btn2OnClick = {
+                                    Toast.makeText(this, "btn2", Toast.LENGTH_SHORT).show()
+                                }
+                            )
+                        }
+                    ) {
+
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AccountBookTheme {
+        AccountBookTopAppBar(
+            title = "Good",
+            btn1Image = R.drawable.ic_left,
+            btn1OnClick = {},
+            btn2Image = R.drawable.ic_right,
+            btn2OnClick = {}
+        )
+    }
+}
