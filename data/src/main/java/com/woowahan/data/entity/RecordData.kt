10a888b678654e71abf74a0be704b7b7273809c7
youngmu <tyg04308@naver.com>
2022-08-01 18:33:33 +0900

    [ADD] #4 - RecordData

---
+++ data/src/main/java/com/woowahan/data/entity/RecordData.kt
@@ -0,0 +1,10 @@
+package com.woowahan.data.entity
+
+data class RecordData(
+    val date: String,
+    val price: Long,
+    val content: String,
+    val payment: String,
+    val category: String,
+    val recordType: String,
+)
