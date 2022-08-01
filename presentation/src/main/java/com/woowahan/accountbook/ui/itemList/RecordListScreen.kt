6985b78a9425d68e3c4abeffd1bc51f2beecb1dc
youngmu <tyg04308@naver.com>
2022-08-01 12:06:09 +0900

    [REFACTOR] #4 - Refactor using new record type

--- presentation/src/main/java/com/woowahan/accountbook/ui/itemList/RecordListScreen.kt
+++ presentation/src/main/java/com/woowahan/accountbook/ui/itemList/RecordListScreen.kt
@@ -7,17 +7,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.woowahan.accountbook.R
 import com.woowahan.accountbook.ui.component.TopAppBar
 import com.woowahan.accountbook.ui.main.MainViewModel
 import com.woowahan.accountbook.ui.navigate.ADD_ITEM
-import com.woowahan.accountbook.ui.navigate.ITEM_LIST
-import com.woowahan.domain.model.RecordItem
+import com.woowahan.domain.model.Record
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -27,11 +29,11 @@ fun RecordListScreen(
 ) {
     val title = viewModel.currentScreen.observeAsState("").value
     val recordViewModel = remember { RecordViewModel() }
-    val records = recordViewModel.records.observeAsState().value!!
+    val records = recordViewModel.records.observeAsState().value
     val leftClicked = recordViewModel.leftBtnOnClick.observeAsState().value!!
     val rightClicked = recordViewModel.rightBtnOnClick.observeAsState().value!!
     var selectMode by remember { mutableStateOf(false) }
-    val selectedItems = remember { mutableStateListOf<RecordItem>() }
+    val selectedItems = remember { mutableStateListOf<Record>() }
 
     Scaffold(
         topBar = {
@@ -93,38 +95,51 @@ fun RecordListScreen(
                     recordViewModel.rightBtnOnClick.postValue(!recordViewModel.rightBtnOnClick.value!!)
                 }
             )
-            LazyColumn {
-                items(
-                    items = records,
-                    itemContent = {
-                        RecordItem(
-                            recordType = it.type,
-                            paymentType = it.payment,
-                            title = it.title,
-                            amount = it.amount,
-                            onClick = {
-                                if (selectMode) {
-                                    if (selectedItems.contains(it)) {
-                                        selectedItems.remove(it)
 
-                                        if (selectedItems.isEmpty()) {
-                                            selectMode = false
+            if (records == null) {
+                Text(
+                    modifier = Modifier.fillMaxSize(),
+                    text = "내역이 없습니다", fontSize = 12.sp, textAlign = TextAlign.Center
+                )
+            } else {
+                if (records.isEmpty()) {
+                    Text(text = "내역이 없습니다", fontSize = 12.sp, textAlign = TextAlign.Center)
+                } else {
+                    LazyColumn {
+                        items(
+                            items = records,
+                            itemContent = {
+                                RecordItem(
+                                    recordType = it.type,
+                                    paymentType = it.payment.name,
+                                    content = it.content,
+                                    price = it.price.toString(),
+                                    category = it.category,
+                                    onClick = {
+                                        if (selectMode) {
+                                            if (selectedItems.contains(it)) {
+                                                selectedItems.remove(it)
+
+                                                if (selectedItems.isEmpty()) {
+                                                    selectMode = false
+                                                }
+                                            } else {
+                                                selectedItems.add(it)
+                                            }
                                         }
-                                    } else {
-                                        selectedItems.add(it)
-                                    }
-                                }
-                            },
-                            onLongClick = {
-                                selectMode = true
-                                if (!selectedItems.contains(it)) {
-                                    selectedItems.add(it)
-                                }
-                            },
-                            isSelected = selectedItems.contains(it),
+                                    },
+                                    onLongClick = {
+                                        selectMode = true
+                                        if (!selectedItems.contains(it)) {
+                                            selectedItems.add(it)
+                                        }
+                                    },
+                                    isSelected = selectedItems.contains(it),
+                                )
+                            }
                         )
                     }
-                )
+                }
             }
         }
     }
