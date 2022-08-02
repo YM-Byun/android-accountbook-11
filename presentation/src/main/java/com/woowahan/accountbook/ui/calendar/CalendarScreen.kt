dc47e2e1799a67fbbb60c2521d8eab808e8fa82b
youngmu <tyg04308@naver.com>
2022-08-02 15:36:02 +0900

    [FEAT] #6 - Calender ui

--- presentation/src/main/java/com/woowahan/accountbook/ui/calendar/CalendarScreen.kt
+++ presentation/src/main/java/com/woowahan/accountbook/ui/calendar/CalendarScreen.kt
@@ -1,30 +1,30 @@
 package com.woowahan.accountbook.ui.calendar
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.woowahan.accountbook.R
-import com.woowahan.accountbook.extenstion.month
-import com.woowahan.accountbook.extenstion.year
 import com.woowahan.accountbook.ui.component.TopAppBar
 import com.woowahan.accountbook.ui.main.MainViewModel
 import com.woowahan.accountbook.ui.main.RecordViewModel
-import com.woowahan.accountbook.ui.theme.Green1
-import com.woowahan.accountbook.ui.theme.Green4
-import java.util.*
+import com.woowahan.accountbook.ui.theme.*
+import com.woowahan.domain.model.Calendar
 
 @Composable
 fun CalendarScreen(
@@ -32,6 +32,9 @@ fun CalendarScreen(
     recordViewModel: RecordViewModel
 ) {
     val title by mainViewModel.appBarTitle.observeAsState("")
+    val calendarViewModel = remember { CalendarViewModel() }
+    calendarViewModel.parseCalendar(title, recordViewModel.records.value!!)
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -50,13 +53,80 @@ fun CalendarScreen(
         Box(
             modifier = Modifier
                 .fillMaxSize()
-                .background(Green4)
                 .padding(it)
         ) {
-            val firstDay = Calendar.getInstance().set(title.year(), title.month(), 1)
-            LazyVerticalGrid(columns = GridCells.Fixed(7)) {
 
+            LazyVerticalGrid(columns = GridCells.Fixed(7)) {
+                items(
+                    items = calendarViewModel.calendarData.value!!
+                ) {
+                    CalendarItem(it)
+                }
             }
+        }
+    }
+}
+
+@Composable
+fun CalendarItem(calendar: Calendar) {
+    val color = if (calendar.isToday) {
+        White
+    } else {
+        Color.Transparent
+    }
+    val modifier = Modifier
+        .fillMaxWidth()
+        .height(60.dp)
+        .background(color)
+        .drawBehind {
+            drawLine(
+                LightPurple,
+                Offset(0f, size.height),
+                Offset(size.width, size.height),
+                1.dp.value
+            )
+            drawLine(
+                LightPurple,
+                Offset(size.width, 0f),
+                Offset(size.width, size.height),
+                1.dp.value
+            )
+        }
+
+    Box(
+        modifier = modifier
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(4.dp)
+        ) {
+            Column {
+                if (calendar.income != "0") {
+                    Text(text = calendar.income, fontSize = 8.sp, color = Green6)
+                }
+                if (calendar.spending != "0") {
+                    Text(text = calendar.spending, fontSize = 8.sp, color = Red)
+                }
+                if (
+                    !(calendar.income == "0" &&
+                            calendar.spending == "0" &&
+                            calendar.total == "0")
+                )
+                    Text(text = calendar.total, fontSize = 8.sp, color = Purple)
+            }
+
+            Text(
+                modifier = Modifier.align(Alignment.BottomEnd),
+                text = calendar.day.toString(),
+                color = if (calendar.isCurrentMonth) {
+                    Purple
+                } else {
+                    Purple40
+                },
+                fontSize = 8.sp,
+                fontWeight = FontWeight.Bold
+            )
         }
     }
 }
