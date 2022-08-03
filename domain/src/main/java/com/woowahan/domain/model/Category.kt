9c19309d79427a4cd3e1bd9fe185905800160372
youngmu <tyg04308@naver.com>
2022-08-03 21:50:54 +0900

    [ADD] #30 - Add id in category, payment

--- domain/src/main/java/com/woowahan/domain/model/Category.kt
+++ domain/src/main/java/com/woowahan/domain/model/Category.kt
@@ -1,6 +1,7 @@
 package com.woowahan.domain.model
 
 data class Category(
+    var id: Int,
     var name: String,
     var color: Int
 )
--- domain/src/main/java/com/woowahan/domain/model/Payment.kt
+++ domain/src/main/java/com/woowahan/domain/model/Payment.kt
@@ -1,5 +1,6 @@
 package com.woowahan.domain.model
 
 data class Payment(
+    var id: Int,
     var name: String
 )
